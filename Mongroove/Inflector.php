7d4d6edb585432c5d03aee5ec374d04efe730260
David Zeller <github@zellerda.com>
2014-10-03 07:39:12 +0200

    Add getCalledClass for old php version

--- Mongroove/Inflector.php
+++ Mongroove/Inflector.php
@@ -32,4 +32,39 @@ class Mongroove_Inflector
     {
         return preg_replace_callback('~(_?)([-_])([\w])~', function($matches){ return $matches[1] . strtoupper($matches[3]); }, ucfirst(strtolower($text)));
     }
+
+    /**
+     * Returns the called class
+     *
+     * @return string
+     */
+    public static function getCalledClass()
+    {
+        $bt = debug_backtrace();
+        $l = 0;
+
+        do
+        {
+            $l++;
+            $lines = file($bt[$l]['file']);
+            $callerLine = $lines[$bt[$l]['line'] - 1];
+
+            preg_match('/([a-zA-Z0-9\_]+)::' . $bt[$l]['function'] . '/', $callerLine, $matches);
+
+            if($matches[1] == 'self')
+            {
+                $line = $bt[$l]['line'] - 1;
+
+                while ($line > 0 && strpos($lines[$line], 'class') === false)
+                {
+                    $line--;
+                }
+
+                preg_match('/class[\s]+(.+?)[\s]+/si', $lines[$line], $matches);
+            }
+        }
+        while ($matches[1] == 'parent' && $matches[1]);
+
+        return $matches[1];
+    }
 }
